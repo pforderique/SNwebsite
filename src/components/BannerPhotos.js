89f2fae81f4e057f1f58cd85ca6ca18a134b5416
Marco Fleming <marcof@mit.edu>
2020-04-13 16:31:30 -0500

    added banner pic

---
+++ src/components/BannerPhotos.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerPhotos = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Photo Gallery</h1>
+            </header>
+            <div className="content">
+                <p>Some of the most memorable moments with the bros</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerPhotos
