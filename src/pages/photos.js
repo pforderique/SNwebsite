2f3e9d724dff3c9e750061c512852187d069c29f
Marco <marcof@mit.edu>
2020-08-08 15:35:07 -0500

    updated some of the pics for photos page

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -6,6 +6,25 @@ import BannerPhotos from '../components/BannerPhotos'
 
 import charlesAndRicky from '../assets/images/charlesAndRicky.jpg'
 import paris from '../assets/images/paris.jpg'
+import pledges from '../assets/images/pledges.jpg'
+import roofpic from '../assets/images/roofpic.jpg'
+import sophFormal from '../assets/images/sophFormal.jpg'
+import sophRetreat from '../assets/images/sophRetreat.jpg'
+import retreatPic1 from '../assets/images/retreatPic1.jpg'
+import sandLetters from '../assets/images/sandLetters.jpg'
+import marathon from '../assets/images/marathon.jpg'
+import charlesPittmanFriend from '../assets/images/charlesPittmanFriend.jpg'
+
+import ajWithIce from '../assets/images/ajWithIce.jpg'
+import brickLetters from '../assets/images/brickLetters.jpg'
+import gusAndEnriko from '../assets/images/gusAndEnriko.jpg'
+import silvieChrisPasqualCharlesLuke from '../assets/images/silvieChrisPasqualCharlesLuke.jpg'
+import moi from '../assets/images/moi.jpg'
+import rushPic2 from '../assets/images/rushPic2.jpg'
+import flag from '../assets/images/flag.jpg'
+
+
+
 
 const Photos = (props) => (
     <Layout>
@@ -17,24 +36,71 @@ const Photos = (props) => (
         <BannerPhotos />
 
 
-        <div id="main">
-            <section id="one" className="tiles">
-                <article style={{backgroundImage: `url(${charlesAndRicky})`}}>
-                    <header className="major">
-                        <h3>Rush</h3>
-                        <p>Discover the brotherhood</p>
-                    </header>
-                    <Link to="/rush" className="link primary"></Link>
-                </article>
-                <article style={{backgroundImage: `url(${paris})`}}>
-                    <header className="major">
-                        <h3>Brothers</h3>
-                        <p>Learn about us</p>
-                    </header>
-                    <Link to="/brothers" className="link primary"></Link>
-                </article>
-            </section>
-            </div>
+                <div id="main">
+                    <section id="one" className="tiles">
+                        <article style={{backgroundImage: `url(${charlesAndRicky})`}}>
+                            <header className="major">
+                                <h3>ΣLΣCTRIC NU</h3>
+                            </header>
+                        </article>
+
+                        <article style={{backgroundImage: `url(${paris})`}}>
+                            <header className="major">
+                                <h3>Brothers in Paris</h3>
+                            </header>
+                        </article>
+
+                        <article style={{backgroundImage: `url(${roofpic})`}}>
+                            <header className="major">
+                                <h3>Brothers and Friends</h3>
+                            </header>
+                        </article>
+
+                        <article style={{backgroundImage: `url(${pledges})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>
+
+                        <article style={{backgroundImage: `url(${sophRetreat})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>
+
+                        <article style={{backgroundImage: `url(${sophFormal})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>                        
+
+                        <article style={{backgroundImage: `url(${retreatPic1})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>                        
+
+                        <article style={{backgroundImage: `url(${sandLetters})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>                        
+
+                        <article style={{backgroundImage: `url(${marathon})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>   
+
+                        <article style={{backgroundImage: `url(${charlesPittmanFriend})`}}>
+                            <header className="major">
+                                <h3>Pledges</h3>
+                            </header>
+                        </article>                           
+
+
+                    </section>
+                    </div>
 
     </Layout>
 )
