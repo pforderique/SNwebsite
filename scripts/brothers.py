6eac92cde3c49bcb77fdf9e06f9e78d4b61b3c2d
Marco <marcof@mit.edu>
2020-08-31 21:04:39 -0500

    python stuff

--- scripts/brothers.py
+++ scripts/brothers.py
@@ -2,6 +2,7 @@
 import os
 
 brothers_path = "../src/assets/images/brothers"
+other_path = "../assets/images/brothers/"
 
 os.chdir(brothers_path)
 
@@ -9,9 +10,11 @@ valid = {".jpg", ".png", ".jpeg"}
 
 for file in os.listdir():
 	if file[-4:] in valid:
-		print("import " + file[:-4] + " from '" + brothers_path + file + "'")
+		print("import " + file[:-4] + " from '" + other_path + file + "'")
+
+print("")
 
 for file in os.listdir():
 	if file[-4:] in valid:
-		print("'" + file[:-4] + "' : "  + file[:-4])
+		print("'" + file[:-4] + "' : "  + file[:-4] + ",")
 
