0adb4a7d4c2ea84b0a7610caf3e1650b5a718fd6
Piero Orderique <porderiq@mit.edu>
2022-08-07 22:06:45 -0400

    added utils

---
+++ src/utils/utils.js
@@ -0,0 +1,18 @@
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * i)
+      const temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+  }
+  
+function intersection(setA, setB) {
+    let _intersection = new Set()
+    for (let elem of setB) {
+        if (setA.has(elem)) {
+            _intersection.add(elem)
+        }
+    }
+    return _intersection
+}
